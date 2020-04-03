8ea6bc1f2e57114552b4fd57eebd75b85c0c196a
Vince Malone <vmalone@twitter.com>
2020-04-03 15:50:57 -0400

    dragon-drop

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ import { init } from './canvas.js';
 const canvas = document.querySelector('#image-canvas');
 const context = canvas.getContext('2d');
 
-const image = new Image();
-image.src = './charizard.png';
-image.onload = () => {
-  const { height, width } = image;
-  canvas.width = width;
-  canvas.height = height;
-  context.imageSmoothingEnabled = false;
-  context.drawImage(image, 0, 0, width, height);
-  const imageData = context.getImageData(0, 0, width, height);
-  init(imageData);
-};
+const dropTarget = document.querySelector('#file-drop-target');
+dropTarget.addEventListener('filedrop', evt => {
+  const [file] = evt.files;
+  go(URL.createObjectURL(file));
+});
+
+go('./charizard.png');
+
+function go(src) {
+  const image = new Image();
+  image.src = src;
+  image.onload = () => {
+    const { height, width } = image;
+    canvas.width = width;
+    canvas.height = height;
+    context.imageSmoothingEnabled = false;
+    context.drawImage(image, 0, 0, width, height);
+    const imageData = context.getImageData(0, 0, width, height);
+    init(imageData);
+  };
+}
